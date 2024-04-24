a18d35b6ef3bcdeebb08937afb53a59f8e5875d4
Samuel Weslley <samuelwrochabarboza@gmail.com>
2024-04-24 00:06:52 -0300

    minor

--- src/app/page/hub/main/main-init/main-init.component.ts
+++ src/app/page/hub/main/main-init/main-init.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginResponse } from 'src/app/shared/model/User.model';
+import { LoginService } from 'src/app/shared/service/login.service';
 
 @Component({
   selector: 'app-main-init',
   templateUrl: './main-init.component.html',
   styleUrls: ['./main-init.component.scss']
 })
-export class MainInitComponent {
+export class MainInitComponent implements OnInit {
+
+  user: LoginResponse | undefined;
+
+  constructor(private loginService: LoginService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getUser()
+  }
+
+  getUser() {
+    var result = this.loginService.getTokenLocalStorage()
+
+    this.user = result;
+  }
 
 }
--- src/app/shared/model/User.model.ts
+++ src/app/shared/model/User.model.ts
@@ -2,4 +2,7 @@ export interface LoginResponse {
     token: string,
     id_family?: number,
     admin?: boolean,
+    login?: string,
+    email?: string,
+    nameFamily?: string,
 }
